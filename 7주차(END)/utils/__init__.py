e069d70545a1817e9a641dff50b19e95ffbe3fae
BJM0203 <70325966+BJM0203@users.noreply.github.com>
2025-05-20 23:05:48 +0900

    Delete 7주차(END) directory

--- 7주차(END)/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .toolInstall import install_if_missing, ensure_requirements
-# 설치 되어있지 않은 패키지 설치
-ensure_requirements()
-
-from .connectWifi import WifiConnector
-from .wifiSecLevel import WifiSecEvaluator
-from .packetAlert import PacketSniff
